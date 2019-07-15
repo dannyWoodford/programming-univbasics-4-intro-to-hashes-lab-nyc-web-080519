83ba252a6b0f066d649e31e94cba3f0737c18771
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-15 19:50:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,14 @@
 def new_hash
-  # return an empty hash
+   {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ hash={:name => "john",
+ :place=> "here"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash=
 end
 
 def id_generator
